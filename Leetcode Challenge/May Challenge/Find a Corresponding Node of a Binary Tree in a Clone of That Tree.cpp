2e60949aadd11e5f0ec2f8917a3d0c4025469292
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-05-17 07:20:39 +0530

    1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ Leetcode Challenge/May Challenge/Find a Corresponding Node of a Binary Tree in a Clone of That Tree.cpp
@@ -0,0 +1,51 @@
+/*
+1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
+
+Given two binary trees original and cloned and 
+given a reference to a node target in the original tree.
+The cloned tree is a copy of the original tree.
+Return a reference to the same node in the cloned tree.
+Note that you are not allowed to change any of the two trees or the target node and 
+the answer must be a reference to a node in the cloned tree.
+
+Input: tree = [7,4,3,null,null,6,19], target = 3
+Output: 3
+Explanation: In all examples the original and cloned trees are shown. The target node is a green node from the original tree. The answer is the yellow node from the cloned tree.
+Example 2:
+
+
+Input: tree = [7], target =  7
+Output: 7
+Example 3:
+
+
+Input: tree = [8,null,6,null,5,null,4,null,3,null,2,null,1], target = 4
+Output: 4
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+The values of the nodes of the tree are unique.
+target node is a node from the original tree and is not null.
+ 
+
+Follow up: Could you solve the problem if repeated values on the tree are allowed?
+*/
+class Solution {
+public:
+    TreeNode* getTargetCopy(TreeNode* orginal, TreeNode* cloned, TreeNode* target) {
+        queue<TreeNode *> org, cln;
+        org.push(orginal); cln.push(cloned);
+        while(org.size()) {
+            orginal = org.front(); org.pop();
+            cloned = cln.front(); cln.pop();
+            if(orginal == target) return cloned;
+            if(orginal->left) org.push(orginal->left);
+            if(cloned->left) cln.push(cloned->left);
+            if(orginal->right) org.push(orginal->right);
+            if(cloned->right) cln.push(cloned->right);
+        }
+        return NULL;
+    }
+};
