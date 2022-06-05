1fcb5fc61c8bbb65904f00a32dd1bf757a210526
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-06-05 07:44:38 +0530

    Small Change

--- Leetcode Challenge/June Challenge/Range Sum Query 2D - Immutable.cpp
+++ Leetcode Challenge/June Challenge/Range Sum Query 2D - Immutable.cpp
@@ -40,14 +40,13 @@ class NumMatrix {
 public:
     vector<vector<int>> sum;
     NumMatrix(vector<vector<int>>& a) {
-        int i, j, row=a.size(), col=(row>0) ? (a[0].size()) : (0);
+        int i, j, row = a.size(), col = (row>0) ? (a[0].size()) : (0);
         sum = vector<vector<int>> (row+1, vector<int> ((col+1), 0));
-        for(i=1;i<=row;i++) {
-            for(j=1;j<=col;j++) {
+        for(i=1; i<=row; i++) {
+            for(j=1; j<=col; j++) {
                 sum[i][j] = a[i-1][j-1] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
             }
         }
-        
     }
     int sumRegion(int row1, int col1, int row2, int col2) {
         return sum[row2+1][col2+1] + sum[row1][col1] - sum[row2+1][col1] - sum[row1][col2+1];
