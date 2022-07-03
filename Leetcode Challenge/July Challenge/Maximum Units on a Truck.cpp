362e8dd754030de679bbd36355a315eb18194b69
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-07-03 10:26:51 +0530

    Small Change

--- Leetcode Challenge/July Challenge/Maximum Units on a Truck.cpp
+++ Leetcode Challenge/July Challenge/Maximum Units on a Truck.cpp
@@ -8,7 +8,6 @@ You are also given an integer truckSize, which is the maximum number of boxes th
 
 Return the maximum total number of units that can be put on the truck.
 
-
 Example 1:
 
 Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
