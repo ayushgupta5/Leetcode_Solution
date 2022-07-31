4ca18060c6eb95b7b296e3f1e3fa802882538ca1
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-07-31 09:38:55 +0530

    Candy

--- Leetcode Challenge/July Challenge/Candy.cpp
+++ Leetcode Challenge/July Challenge/Candy.cpp
@@ -21,7 +21,6 @@ Input: ratings = [1,2,2]
 Output: 4
 Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
 The third child gets 1 candy because it satisfies the above two conditions.
- 
 
 Constraints:
 
