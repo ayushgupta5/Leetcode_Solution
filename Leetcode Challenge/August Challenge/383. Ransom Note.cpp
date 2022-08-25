2748bd80d433ce0ec0efa6637f67649dff9371ca
Ayush Gupta <ayushgupta.ac@gmail.com>
2022-08-25 06:04:19 +0530

    383. Ransom Note

---
+++ Leetcode Challenge/August Challenge/383. Ransom Note.cpp
@@ -0,0 +1,38 @@
+/*383. Ransom Note
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+ 
+
+Constraints:
+
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+*/
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> A(26, 0);
+        for(auto x: magazine) A[x-'a']++;
+        for(auto x: ransomNote) {
+            if(A[x-'a']<=0) return 0;
+            A[x-'a']--;
+        }
+        return 1;
+    }
+};
